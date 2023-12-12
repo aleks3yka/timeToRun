283506ed824c69a17cab80be24b40866df9c7cfb
lazaretto <maks.sinelnikov2007@gmail.com>
2023-12-12 15:52:54 +0300

    empty commit

---
+++ core/src/com/mygdx/game/utils/SoundsHelper.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.math.MathUtils;
+
+public class SoundsHelper {
+    static Music backSound = Gdx.audio.newMusic(Gdx.files.internal("sounds/MenuMusic.mp3"));
+
+    /*static Sound[] TimetoRun = {
+            Gdx.audio.newSound(Gdx.files.internal("sounds/.mp3")),
+            Gdx.audio.newSound(Gdx.files.internal("sounds/.mp3"))
+    };*/
+
+    public static void playBackSound() {
+        backSound.play();
+        backSound.setLooping(true);
+    }
+
+    public static void stopPlaying() {
+        backSound.stop();
+    }
+
+    /*public static void playMosquitoSound() {
+        TimetoRun[MathUtils.random(0, TimetoRun.length - 1)].play();
+    }*/
+}
